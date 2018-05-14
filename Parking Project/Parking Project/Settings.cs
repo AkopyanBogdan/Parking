2b6c57588da294d175cf69c4bd005bb08454a9e6
Bohdan <akopyan.bogdan@gmail.com>
2018-05-15 01:24:05 +0300

    Fixe bugs in Settings class fixes #17

--- Parking Project/Parking Project/Settings.cs
+++ Parking Project/Parking Project/Settings.cs
@@ -8,48 +8,17 @@ namespace Parking_Project
 {
     public static class Settings
     {
-        public static readonly int ParkingSpace;
-        private static double _fine;
+        public static readonly int _parkingSpace = 20;
+        public static readonly double _fine = 1.1;
+        public static readonly int _timeout = 3000;
+        public static readonly string url = "Transactions.log";
 
-        private static readonly Dictionary<string, int> _price = new Dictionary<string, int>
+        public static readonly Dictionary<string, int> _price = new Dictionary<string, int>
         {
             {CarType.Truck.ToString(), 5 },
             {CarType.Passenger.ToString(), 3 },
             {CarType.Bus.ToString(), 2 },
             {CarType.Motorcycle.ToString(), 1 }
         };
-
-        public static double Fine
-        {
-            get { return _fine; }
-            set
-            {
-                if (value <= 0)
-                    _fine = 0;
-                else
-                    _fine = value;
-            }
-        }
-
-        static Settings()
-        {
-            ParkingSpace = 20;
-        }
-
-        //Свойство Timeout(каждые N-секунд списывает средства за парковочное место) - по умолчанию 3 секунды
-        public static double Timeout(bool hasMoney, string carType)
-        {
-            int result;
-            double sum = 0;
-
-            _price.TryGetValue(carType, out result);
-
-            if (hasMoney)
-                sum = result;
-            else
-                sum = result + result * 100 / Fine;
-
-            return sum;
-        }
     }
 }
